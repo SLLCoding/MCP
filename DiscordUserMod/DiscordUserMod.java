2c3f581e8e08199d25c2c547ca3df625b33b4d0e
SLL <39528861+SuperLegoLuis@users.noreply.github.com>
2019-11-30 10:43:44 +0000

    Create DiscordUserMod.java

---
+++ DiscordUserMod/DiscordUserMod.java
@@ -0,0 +1,32 @@
+package your.package;
+
+public class DiscordUserMod extends ModDraggable {
+
+	@Override
+	public int getWidth() {
+		return font.getStringWidth([Data Class].getDiscordUsername());
+	}
+
+	@Override
+	public int getHeight() {
+		return font.FONT_HEIGHT;
+	}
+  
+  // IMPORTANT: Requires a variable with the username stored inside, this is set in the DiscordRP#init().
+	@Override
+	public void render(ScreenPosition pos) {
+		if ([Data Class].getDiscordUsername() != null) {
+			font.drawString("Discord: " + [Data Class].getDiscordUsername(), pos.getAbsoluteX(), pos.getAbsoluteY(), -1);
+		}
+		else {
+      // Isn't using Discord.
+			font.drawString("", pos.getAbsoluteX(), pos.getAbsoluteY(), -1);
+		}
+	}
+	
+	@Override
+	public void renderDummy(ScreenPosition pos) {
+		font.drawString([Data Class].getDiscordUsername(), pos.getAbsoluteX(), pos.getAbsoluteY(), -1);
+	}
+
+}
